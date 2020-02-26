59e4556e406382f8d010ee4b4f3310c4311dfd0d
muguifang <mum68481@163.com>
2020-02-26 23:00:02 +0800

    用户主页、全部音乐页面

--- client/watermelon-user/src/components/head.vue
+++ client/watermelon-user/src/components/head.vue
@@ -1,21 +1,25 @@
 <template>
-  <div style="margin-bottom: 20px;">
-    <div>
+  <div id="head-wrap">
+    <div class="user-info">
       <el-link :underline="false">登录</el-link>
       <el-link :underline="false">注册</el-link>
-      <el-link icon="el-icon-message">网站建议</el-link>
+      <el-link :underline="false" icon="el-icon-message">网站建议</el-link>
     </div>
-    <div>
-      <el-input
-        placeholder="请输入内容"
-        style="width: 250px; display: inline-block;"
-        v-model="input"
-        clearable
-      >
-      </el-input>
-      <el-button type="info" icon="el-icon-search"></el-button>
+    <div class="web-info">
+      <img src="@/assets/xgImg.png" class="xgImg xg" />
+      <label>音乐网</label>
+      <div class="search-info">
+        <el-input
+          placeholder="请输入内容"
+          style="width: 250px; display: inline-block;"
+          v-model="input"
+          clearable
+        >
+        </el-input>
+        <el-button type="info" icon="el-icon-search">搜索</el-button>
+      </div>
     </div>
-    <div>
+    <div class="nav-wrap">
       <el-menu
         :default-active="routerPath == '/index' ? '/' : routerPath"
         :select="handleSelect()"
@@ -55,4 +59,52 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#head-wrap {
+  margin-bottom: 20px;
+}
+.user-info {
+  width: 100%;
+  height: 45px;
+  text-align: right;
+  .el-link {
+    padding-right: 20px;
+    font-size: 18px;
+  }
+}
+.web-info {
+  .xgImg {
+    width: 50px;
+    position: absolute;
+    top: 25px;
+    left: 100px;
+  }
+  label {
+    position: absolute;
+    top: 35px;
+    left: 160px;
+    font-size: 30px;
+  }
+}
+.search-info {
+  position: relative;
+  top: -16px;
+  left: 890px;
+  width: 22%;
+  .el-input {
+    padding-right: 10px;
+  }
+  .el-button {
+    height: 40px;
+  }
+}
+.nav-wrap {
+  .el-menu {
+    padding-left: 400px;
+    .el-menu-item {
+      margin-right: 50px;
+      font-size: 18px;
+    }
+  }
+}
+</style>
--- client/watermelon-user/src/views/music/index.vue
+++ client/watermelon-user/src/views/music/index.vue
@@ -1,12 +1,73 @@
 <template>
-  <div>
-    <div style="width: 800px;">
+  <div id="index-wrap">
+    <div class="lbt-card">
       <el-carousel :interval="4000" type="card" height="200px">
         <el-carousel-item v-for="url in urls" :key="url">
           <el-image :src="url"></el-image>
         </el-carousel-item>
       </el-carousel>
     </div>
+    <div class="latest-info">
+      <div class="flag"></div>
+      <label>最新歌单</label>
+      <el-divider></el-divider>
+      <div class="music-info">
+        <el-row>
+          <el-col
+            :span="4"
+            v-for="(o, index) in 10"
+            :key="o"
+            :offset="index > 0 ? 10 : 0"
+          >
+            <el-card :body-style="{ padding: '0px' }">
+              <img src="@/assets/login.jpg" class="image" />
+              <div style="padding: 14px;">
+                <span>好听的音乐</span>
+                <div class="bottom clearfix">
+                  <div v-for="o in 5" :key="o" class="star">
+                    <i class="el-icon-star-off"></i>
+                  </div>
+                  <el-button type="text" class="button">查看音乐</el-button>
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <div class="latest-info">
+      <div class="flag"></div>
+      <label>推荐歌单</label>
+      <el-divider></el-divider>
+      <div class="music-info">
+        <el-row>
+          <el-col
+            :span="4"
+            v-for="(o, index) in 10"
+            :key="o"
+            :offset="index > 0 ? 10 : 0"
+          >
+            <el-card :body-style="{ padding: '0px' }">
+              <img src="@/assets/login.jpg" class="image" />
+              <div style="padding: 14px;">
+                <span>好听的音乐</span>
+                <div class="bottom clearfix">
+                  <div v-for="o in 5" :key="o" class="star">
+                    <i class="el-icon-star-off"></i>
+                  </div>
+                  <el-button type="text" class="button">查看音乐</el-button>
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <!-- <div class="latest-info">
+      <div class="flag"></div>
+      <label>推荐歌单</label>
+      <el-divider></el-divider>
+    </div> -->
   </div>
 </template>
 <script>
@@ -29,6 +90,29 @@ export default {
 };
 </script>
 <style>
+.index-wrap {
+  width: 100%;
+}
+.latest-info {
+  width: 80%;
+  margin: 0 auto;
+}
+label {
+  font-size: 22px;
+  letter-spacing: 3px;
+  margin-left: 10px;
+}
+.flag {
+  width: 15px;
+  height: 15px;
+  border: 5px solid red;
+  border-radius: 50%;
+  float: left;
+}
+.lbt-card {
+  width: 800px;
+  margin: 0 auto;
+}
 .el-carousel__item h3 {
   color: #475669;
   font-size: 14px;
@@ -43,5 +127,38 @@ export default {
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+.music-info {
+  width: 100%;
+}
+.el-row {
+  width: 100%;
+  float: right;
+}
+.el-col {
+  margin-right: 25px;
+  margin-bottom: 30px;
+  margin-left: 19px;
+}
+.image {
+  width: 100%;
+}
+.bottom {
+  height: 44px;
+  line-height: 44px;
+}
+.el-button {
+  float: right;
+}
+.star {
+  display: inline;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
 }
 </style>
