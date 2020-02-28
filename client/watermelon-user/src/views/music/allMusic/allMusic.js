9533aff21abf8b948fbb188c9c67eebb6abba22e
red-almond <qblsiwen@163.com>
2020-02-28 20:13:04 +0800

    提交获取所有音乐类别

--- client/watermelon-user/src/views/music/allMusic/allMusic.js
+++ client/watermelon-user/src/views/music/allMusic/allMusic.js
@@ -1,12 +1,15 @@
 // -- 引入外部其他文件 --// -- 引入外部其他文件 --
+import { getAllType, getMusicByType } from "@/api/music.js";
 // -- 名字 --
 
-const name = "";
+const name = "AllMusic";
 
 // -- 数据 --
 
 const data = function() {
-  return {};
+  return {
+    typeList: []
+  };
 };
 
 // -- 方法 --
@@ -15,11 +18,38 @@ const methods = {
   //页面内路由跳转
   toView() {
     this.$router.push("/musicInfo");
+  },
+  //点击类别获取该类别下音乐
+  getMusic(param){
+    getMusicByType(param).then(response => {
+      const res = response.data;
+      console.log(res);
+    });
   }
 };
 
 // -- 页面加载完成 --
-const created = function() {};
+const created = function() {
+  this.typeList = [];
+  const param = { typename: "" };
+  getAllType(param).then(response => {
+    const res = response.data;
+    if (res.code === 200) {
+      const typeData = res.data;
+      for (let i = 0; i < typeData.length; i++) {
+        const type = {
+          id: "",
+          name: ""
+        };
+        type.id = typeData[i].id;
+        type.name = typeData[i].typename;
+        this.typeList.push(type);
+      }
+    } else {
+      this.$message.warning("初始化页面失败!");
+    }
+  });
+};
 
 // -- 自动计算属性 --
 
